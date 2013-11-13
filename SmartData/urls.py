7fd95d8b50ee732e6bcc2905aa5e6302a9325db5
Robin Wu <bcc0421@gmail.com>
2013-11-13 10:29:51 +0800

    Add static files.

--- SmartData/urls.py
+++ SmartData/urls.py
@@ -1,17 +1,21 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from SmartDataApp.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'SmartData.views.home', name='home'),
-    # url(r'^SmartData/', include('SmartData.foo.urls')),
+                       # Examples:
+                       # url(r'^$', 'SmartData.views.home', name='home'),
+                       # url(r'^SmartData/', include('SmartData.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+                       # Uncomment the next line to enable the admin:
+                       # url(r'^admin/', include(admin.site.urls)),
+
+                       url(r'^$', index),
+                       url(r'^register$', register),
 )
--- SmartDataApp/views.py
+++ SmartDataApp/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+#coding:utf-8
+from django.shortcuts import render_to_response
+from django.contrib.auth.models import User
+
+
+def index(request):
+    return render_to_response('index.html')
+
+
+def login(request):
+    pass
+
+
+def register(request):
+    if request.method == 'GET':
+        return render_to_response('register.html')
+    elif request.method == 'POST':
+        username = 'username'
+        password = 'password'
+        email = 'email'
+        user = User.objects.create_user(username, email, password)
+        user.save()
