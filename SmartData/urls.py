9cc55a8d1986d82bdad9a016c70a00626ada78c3
bcc0421 <bcc0421@gmail.com>
2013-11-14 17:04:19 +0800

    Add update profile page.

--- SmartData/urls.py
+++ SmartData/urls.py
@@ -12,4 +12,5 @@ urlpatterns = patterns('',
                        url(r'^login/$', login),
                        url(r'^logout/$', logout),
                        url(r'^dashboard/$', dashboard),
+                       url(r'^profile/$', profile),
 )
