d808688f725941e42d43aa42fefcd9f537c6a091
bcc0421 <bcc0421@gmail.com>
2013-11-14 15:42:27 +0800

    Add index page.

--- SmartData/urls.py
+++ SmartData/urls.py
@@ -6,16 +6,8 @@ from SmartDataApp.views import *
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'SmartData.views.home', name='home'),
-                       # url(r'^SmartData/', include('SmartData.foo.urls')),
-
-                       # Uncomment the admin/doc line below to enable admin documentation:
-                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-                       # Uncomment the next line to enable the admin:
-                       # url(r'^admin/', include(admin.site.urls)),
-
                        url(r'^$', index),
+                       url(r'^index/$', index),
                        url(r'^register/$', register),
+                       url(r'^login/$', login)
 )
